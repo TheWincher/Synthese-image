1ba30033d35268b51a868db7d60f5e909ca75941
Emmanuel Louchez <louchez26@hotmail.fr>
2019-09-20 16:08:48 +0200

    il y a du mieux dans le tp2

--- Synthese_img/TP2/TP2.cpp
+++ Synthese_img/TP2/TP2.cpp
@@ -6,8 +6,8 @@ int main() {
 	std::vector<Sphere> spheres;
 
 	//On ajoute des spheres dans le tableau
-	spheres.push_back(Sphere(Vector3<float>(200,200,300),100));
-	spheres.push_back(Sphere(Vector3<float>(400,400,300),50 ));
+	spheres.push_back(Sphere(Vector3<float>(400,400,300),100));
+	spheres.push_back(Sphere(Vector3<float>(200,200,300),50 ));
 
 	//Création du rayon et de la lumière
 	Light light = { Vector3<float>(100,0,300),Vector3<int>(255, 255, 255) };
@@ -26,6 +26,10 @@ int main() {
 			for (std::vector<Sphere>::iterator it = spheres.begin(); it != spheres.end(); it++) {
 				std::optional<float> t = intersectionRayonSphere(*it, r);
 
+				/*if (x == 390 && y == 306) {
+					std::cout << "-----------------------------" << std::endl;
+					std::cout << t.has_value() << std::endl;
+				}*/
 				// Cas où il y a une intersection
 				if (t.has_value()) {
 
@@ -36,53 +40,33 @@ int main() {
 					Vector3<float> L = light.pos - pointIntersection;
 					Vector3<float> N = it->getNormal(pointIntersection);
 
-					Ray r2 = { pointIntersection, L.normalized() };
+					Ray r2 = { pointIntersection + L.normalized() * 0.01F, L.normalized() };
+					std::optional<float> t2;
+
 					for (std::vector<Sphere>::iterator it2 = spheres.begin(); it2 != spheres.end(); it2++) {
 
-						std::optional<float> t2 = intersectionRayonSphere(*it2, r2);
-						if (t2.has_value() && (pointIntersection + L.normalized() * t2.value()).norm() < L.norm()) {
-							ppm.pixelMatrix[y][x] = Vector3<int>(255, 0, 0);
-							break;
+						if (x == 390 && y == 306) std::cout << t.has_value() << std::endl;
+						if (t2.has_value()) {
+							std::optional<float> tt = intersectionRayonSphere(*it2, r2);
+							if (tt.has_value() && tt.value() < t2.value() && (L.normalized() * tt.value()).norm() > FLT_EPSILON)
+								t2 = tt;
 						}
 						else {
-
-							float dt = L.normalized().dot(N.normalized());
-							float dist = L.norm();
-
-							Vector3<int> color(light.color.x * dt, light.color.y * dt, light.color.z * dt);
-							ppm.pixelMatrix[y][x] = color;
+							t2 = intersectionRayonSphere(*it2, r2);
 						}
 					}
-					
 
-					/*Ray r2 = { pointIntersection, L.normalized() };
-					for (std::vector<Sphere>::iterator it2 = spheres.begin(); it2 != spheres.end(); it2++) {
-						std::optional<float> t2 = intersectionRayonSphere(*it2, r2);
-						if (t2.has_value() && (pointIntersection + (L.position - pointIntersection).normalized() * t2.value()).norm() < (L.position - pointIntersection).norm()) {
-							//intersection avant la lumiere donc dans l'ombre
-							ppm.pixelMatrix[row][col] = Vector3<int>(0, 0, 0);
-							break;
-						}
-						else {
-							Vector3<float> lightDirection = (L.position - pointIntersection);
-							Vector3<float> normale = (pointIntersection - it->center);
+					if (t2.has_value() && (L.normalized() * t2.value()).norm() < L.norm())
+						ppm.pixelMatrix[y][x] = Vector3<int>(0, 0, 0);
+					else {
 
-							float angle = (lightDirection.dot(normale)) / (lightDirection.norm() * normale.norm());
-							float dist = lightDirection.norm();
+						float dt = L.normalized().dot(N.normalized());
+						float dist = L.norm();
 
-							Vector3<float> Lres = Vector3<float>(angle, angle, angle) * L.intensite;
-							ppm.pixelMatrix[row][col] = Vector3<int>(Lres.x, Lres.y, Lres.z);
-							//ppm.pixelMatrix[row][col] = Vector3<int>(255, 255, 255); 
-						}
-					}	*/	
+						Vector3<int> color(light.color.x * dt, light.color.y * dt, light.color.z * dt);
+						ppm.pixelMatrix[y][x] = color;
+					}
 				}
-				/*else {
-					ppm.pixelMatrix[row][col] = ppm.pixelMatrix[row][col] + Vector3<int>(0, 0, 0);
-				}*/
-
-				/*out << (int) pixels[y][x].r << std::endl;
-				out << (int) pixels[y][x].g << std::endl;
-				out << (int) pixels[y][x].b << std::endl;*/
 			}		
 		}
 	}
