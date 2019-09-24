94b46e1d3806d84055f816908ec9c7590c5d99a6
Emmanuel Louchez <louchez26@hotmail.fr>
2019-09-24 11:51:57 +0200

    ajout de plusieurs source de lumière et des couleurs

--- Synthese_img/TP2/TP2.cpp
+++ Synthese_img/TP2/TP2.cpp
@@ -1,16 +1,21 @@
 #include "TP2.hpp"
+#include <time.h>
 
+#pragma exp parallel for
 int main() {
 
 	//Création d'un tableau de spheres
 	std::vector<Sphere> spheres;
+	srand(time(NULL));
 
 	//On ajoute des spheres dans le tableau
-	spheres.push_back(Sphere(Vector3<float>(400,400,300),100));
-	spheres.push_back(Sphere(Vector3<float>(200,200,300),50 ));
+	spheres.push_back(Sphere(Vector3<float>(300,300,300),50));
+	spheres.push_back(Sphere(Vector3<float>(600,600,600),250 ));
 
 	//Création du rayon et de la lumière
-	Light light = { Vector3<float>(100,0,300),Vector3<int>(255, 255, 255) };
+	std::vector<Light> lights;
+	lights.push_back(Light(Vector3<float>(100, 100, 0), Vector3<float>(20, 20, 10), Vector3<int>(0, 0, 255), 300000));
+	lights.push_back(Light(Vector3<float>(100, 500, 0), Vector3<float>(20, 20, 10), Vector3<int>(255, 255, 0), 200000));
 
 	//Création d'une image 
 	PPM ppm(WIDTH, HEIGHT, 255);
@@ -22,52 +27,45 @@ int main() {
 			//On envoit un rayon
 			Ray r = { Vector3<float>(x,y,0),Vector3<float>(0,0,1) };
 
-			//On regarde si il y a une intersection avec chaque object de la scène
-			for (std::vector<Sphere>::iterator it = spheres.begin(); it != spheres.end(); it++) {
-				std::optional<float> t = intersectionRayonSphere(*it, r);
+			Sphere *s = (Sphere *) malloc(sizeof(struct Sphere));
+			std::optional<float> t = getNearestIntersect(spheres, r, s);
 
-				/*if (x == 390 && y == 306) {
-					std::cout << "-----------------------------" << std::endl;
-					std::cout << t.has_value() << std::endl;
-				}*/
-				// Cas où il y a une intersection
-				if (t.has_value()) {
+			// Cas où il y a une intersection
+			if (t.has_value()) {
 
-					//On calcul le point d'intersection
-					Vector3<float> pointIntersection = r.origin + r.direction * t.value();
+				//On calcul le point d'intersection
+				Vector3<float> pointIntersection = r.origin + r.direction * t.value();
 
-					//
-					Vector3<float> L = light.pos - pointIntersection;
-					Vector3<float> N = it->getNormal(pointIntersection);
+				//
+				Vector3<float> N = s->getNormal(pointIntersection);
+				Vector3<float> color(0,0,0);
 
-					Ray r2 = { pointIntersection + L.normalized() * 0.01F, L.normalized() };
-					std::optional<float> t2;
+				for (int l = 0; l < lights.size(); l++) {
+					for (int i = 0; i < NB_POINT_LIGHT; i++) {
+						float xRand = rand() % ((int)lights[l].offset.x) + 1;
+						float yRand = rand() % ((int)lights[l].offset.y) + 1;
+						float zRand = rand() % ((int)lights[l].offset.z) + 1;
 
-					for (std::vector<Sphere>::iterator it2 = spheres.begin(); it2 != spheres.end(); it2++) {
+						Vector3<float> vRand(xRand,yRand, zRand);
+						Vector3<float> L = (lights[l].pos + vRand) - pointIntersection;
+						Vector3<float> Lnormalized = L.normalized();
 
-						if (x == 390 && y == 306) std::cout << t.has_value() << std::endl;
-						if (t2.has_value()) {
-							std::optional<float> tt = intersectionRayonSphere(*it2, r2);
-							if (tt.has_value() && tt.value() < t2.value() && (L.normalized() * tt.value()).norm() > FLT_EPSILON)
-								t2 = tt;
-						}
+						Ray r2 = { pointIntersection + Lnormalized * 0.01F, Lnormalized };
+						std::optional<float> t2 = getNearestIntersect(spheres, r2, nullptr);
+
+						if (t2.has_value() && (Lnormalized * t2.value()).norm() < L.norm())
+							color = color + Vector3<float>(0, 0, 0);
 						else {
-							t2 = intersectionRayonSphere(*it2, r2);
+							float dt = Lnormalized.dot(N.normalized());
+							float dist = L.norm();
+							if (dt < 0) dt = 0;
+
+							color = color + Vector3<float>(lights[l].color.x * dt, lights[l].color.y * dt, lights[l].color.z * dt) * lights[l].intensity / (NB_POINT_LIGHT * dist * dist);
 						}
-					}
-
-					if (t2.has_value() && (L.normalized() * t2.value()).norm() < L.norm())
-						ppm.pixelMatrix[y][x] = Vector3<int>(0, 0, 0);
-					else {
-
-						float dt = L.normalized().dot(N.normalized());
-						float dist = L.norm();
-
-						Vector3<int> color(light.color.x * dt, light.color.y * dt, light.color.z * dt);
-						ppm.pixelMatrix[y][x] = color;
 					}
 				}
-			}		
+				ppm.pixelMatrix[y][x] = Vector3<int>((int)clamp(0,255,color.x), (int)clamp(0,255,color.y), (int)clamp(0,255,color.z));
+			}
 		}
 	}
 
--- Synthese_img/TP2/TP2.hpp
+++ Synthese_img/TP2/TP2.hpp
@@ -10,6 +10,7 @@
 
 #define WIDTH 600
 #define HEIGHT 600
+#define NB_POINT_LIGHT 100
 
 struct Sphere {
 	Vector3<float> center;
@@ -29,8 +30,16 @@ struct Ray {
 struct Light
 {
 	Vector3<float> pos;
+	Vector3<float> offset;
 	Vector3<int> color;
 	float intensity;
+
+	Light(Vector3<float> p, Vector3<float> o, Vector3<int> c, float i) {
+		pos = p;
+		offset = o;
+		color = c;
+		intensity = i;
+	}
 };
 
 std::optional<float> intersectionRayonSphere(const Sphere& s, const Ray& r) {
@@ -51,5 +60,33 @@ std::optional<float> intersectionRayonSphere(const Sphere& s, const Ray& r) {
 	if (res < 0)
 		return std::nullopt;
 	return res;
+}
+
+std::optional<float> getNearestIntersect(std::vector<Sphere> spheres, const Ray& r, Sphere *s) {
+	std::optional<float> res = std::nullopt;
+
+	for (std::vector<Sphere>::iterator it2 = spheres.begin(); it2 != spheres.end(); it2++) {
+		if (res.has_value()) {
+			std::optional<float> tt = intersectionRayonSphere(*it2, r);
+			if (tt.has_value() && tt.value() < res.value()) {
+				res = tt;
+				if (s != nullptr)  *s = *it2;	
+			}
+		}
+		else {
+			res = intersectionRayonSphere(*it2, r);
+			if (res.has_value() && s != nullptr) {
+				*s = *it2;
+			}
+		}
+	}
+	return res;
+}
+
+float clamp(float min, float max, float value)
+{
+	if (value < min) return min;
+	if (value > max) return max;
+	return value;
 }
 #endif
